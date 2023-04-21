6f37ed418ca4aea83441b08cce1349859ad9b5eb
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-04-20 22:18:47 -0300

    Commit 22

--- lib/utils/cardapio.dart
+++ lib/utils/cardapio.dart
@@ -1,245 +1,187 @@
-List<Map<String, dynamic>> harburgueriaCardapio = [
-  {
-    "nome": "Hambúrguer Clássico",
-    "itens": [
-      {
-        "nome": "pão",
-        "opcional": false,
-      },
-      {
-        "nome": "carne",
-        "opcional": true,
-      },
-      {
-        "nome": "queijo",
-        "opcional": true,
-      },
-      {
-        "nome": "alface",
-        "opcional": true,
-      },
-      {
-        "nome": "tomate",
-        "opcional": true,
-      },
-      {
-        "nome": "cebola",
-        "opcional": true,
-      },
-      {
-        "nome": "ketchup",
-        "opcional": true,
-      },
-      {
-        "nome": "mostarda",
-        "opcional": true,
-      },
+class HarburgueriaCardapioItem {
+  final String nome;
+  final bool opcional;
+
+  HarburgueriaCardapioItem({
+    required this.nome,
+    required this.opcional,
+  });
+}
+
+class HarburgueriaCardapio {
+  final String nome;
+  final List<HarburgueriaCardapioItem> itens;
+  final String categoria;
+  final double preco;
+  final List<String> galeria;
+
+  HarburgueriaCardapio({
+    required this.nome,
+    required this.itens,
+    required this.categoria,
+    required this.preco,
+    required this.galeria,
+  });
+}
+
+List<HarburgueriaCardapio> a = [
+  HarburgueriaCardapio(
+    nome: "Hambúrguer Clássico",
+    itens: [
+      HarburgueriaCardapioItem(nome: "pão", opcional: false),
+      HarburgueriaCardapioItem(nome: "carne", opcional: true),
+      HarburgueriaCardapioItem(nome: "queijo", opcional: true),
+      HarburgueriaCardapioItem(nome: "alface", opcional: true),
+      HarburgueriaCardapioItem(nome: "tomate", opcional: true),
+      HarburgueriaCardapioItem(nome: "cebola", opcional: true),
+      HarburgueriaCardapioItem(nome: "ketchup", opcional: true),
+      HarburgueriaCardapioItem(nome: "mostarda", opcional: true),
     ],
-    "categoria": "Tradicional",
-    "preco": 20.00,
-    "galeria": [],
-  },
+    categoria: "Tradicional",
+    preco: 20.00,
+    galeria: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+    ],
+  ),
+];
+
+List<Map<String, dynamic>> harburgueriaCardapio = [
   {
     "nome": "Cheeseburger",
     "itens": [
-      {
-        "nome": "pão",
-        "opcional": false,
-      },
-      {
-        "nome": "carne",
-        "opcional": true,
-      },
-      {
-        "nome": "queijo",
-        "opcional": true,
-      },
-      {
-        "nome": "alface",
-        "opcional": true,
-      },
-      {
-        "nome": "tomate",
-        "opcional": true,
-      },
-      {
-        "nome": "cebola",
-        "opcional": true,
-      },
-      {
-        "nome": "molho especial da casa",
-        "opcional": true,
-      },
+      {"nome": "pão", "opcional": false},
+      {"nome": "carne", "opcional": true},
+      {"nome": "queijo", "opcional": true},
+      {"nome": "alface", "opcional": true},
+      {"nome": "tomate", "opcional": true},
+      {"nome": "cebola", "opcional": true},
+      {"nome": "molho especial da casa", "opcional": true},
     ],
     "categoria": "Tradicional",
     "preco": 18.00,
-    "galeria": [],
+    "galeria": [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+    ],
   },
   {
     "nome": "Hambúrguer com Bacon",
     "itens": [
-      {
-        "nome": "pão",
-        "opcional": false,
-      },
-      {
-        "nome": "carne",
-        "opcional": true,
-      },
-      {
-        "nome": "queijo",
-        "opcional": true,
-      },
-      {
-        "nome": "bacon crocante",
-        "opcional": true,
-      },
-      {
-        "nome": "alface",
-        "opcional": true,
-      },
-      {
-        "nome": "tomate",
-        "opcional": true,
-      },
-      {
-        "nome": "cebola",
-        "opcional": true,
-      },
-      {
-        "nome": "molho barbecue",
-        "opcional": true,
-      },
+      {"nome": "pão", "opcional": false},
+      {"nome": "carne", "opcional": true},
+      {"nome": "queijo", "opcional": true},
+      {"nome": "bacon crocante", "opcional": true},
+      {"nome": "alface", "opcional": true},
+      {"nome": "tomate", "opcional": true},
+      {"nome": "cebola", "opcional": true},
+      {"nome": "molho barbecue", "opcional": true},
     ],
     "categoria": "Tradicional",
     "preco": 23.00,
-    "galeria": [],
+    "galeria": [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+    ],
   },
   {
     "nome": "Hambúrguer de Picanha",
     "itens": [
-      {
-        "nome": "pão",
-        "opcional": false,
-      },
-      {
-        "nome": "carne de picanha",
-        "opcional": true,
-      },
-      {
-        "nome": "queijo",
-        "opcional": true,
-      },
-      {
-        "nome": "alface",
-        "opcional": true,
-      },
-      {
-        "nome": "tomate",
-        "opcional": true,
-      },
-      {
-        "nome": "cebola caramelizada",
-        "opcional": true,
-      },
-      {
-        "nome": "molho especial da casa",
-        "opcional": true,
-      },
+      {"nome": "pão", "opcional": false},
+      {"nome": "carne de picanha", "opcional": true},
+      {"nome": "queijo", "opcional": true},
+      {"nome": "alface", "opcional": true},
+      {"nome": "tomate", "opcional": true},
+      {"nome": "cebola caramelizada", "opcional": true},
+      {"nome": "molho especial da casa", "opcional": true},
     ],
-    "categoria": "",
-    "preco": "",
-    "galeria": [],
+    "categoria": "Especial",
+    "preco": 25.00,
+    "galeria": [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+    ],
   },
   {
     "nome": "Hambúrguer Vegetariano",
     "itens": [
-      {
-        "nome": "pão",
-        "opcional": false,
-      },
-      {
-        "nome": "hambúrguer de soja",
-        "opcional": true,
-      },
-      {
-        "nome": "queijo",
-        "opcional": true,
-      },
-      {
-        "nome": "alface",
-        "opcional": true,
-      },
-      {
-        "nome": "tomate",
-        "opcional": true,
-      },
-      {
-        "nome": "cebola",
-        "opcional": true,
-      },
-      {
-        "nome": "maionese vegana",
-        "opcional": true,
-      },
+      {"nome": "pão", "opcional": false},
+      {"nome": "hambúrguer de soja", "opcional": true},
+      {"nome": "queijo", "opcional": true},
+      {"nome": "alface", "opcional": true},
+      {"nome": "tomate", "opcional": true},
+      {"nome": "cebola", "opcional": true},
+      {"nome": "maionese vegana", "opcional": true},
     ],
-    "categoria": "",
-    "preco": "",
-    "galeria": [],
+    "categoria": "Especial",
+    "preco": 28.00,
+    "galeria": [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+    ],
   },
   {
     "nome": "Hambúrguer de Frango",
     "itens": [
-      {
-        "nome": "pão",
-        "opcional": false,
-      },
-      {
-        "nome": "filé de frango empanado",
-        "opcional": true,
-      },
-      {
-        "nome": "queijo",
-        "opcional": true,
-      },
-      {
-        "nome": "alface",
-        "opcional": true,
-      },
-      {
-        "nome": "tomate",
-        "opcional": true,
-      },
-      {
-        "nome": "cebola",
-        "opcional": true,
-      },
+      {"nome": "pão", "opcional": false},
+      {"nome": "filé de frango empanado", "opcional": true},
+      {"nome": "queijo", "opcional": true},
+      {"nome": "alface", "opcional": true},
+      {"nome": "tomate", "opcional": true},
+      {"nome": "cebola", "opcional": true},
       "maionese especial"
     ],
-    "categoria": "",
-    "preco": "",
-    "galeria": [],
+    "categoria": "Especial",
+    "preco": 29.00,
+    "galeria": [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+    ],
   },
   {
     "nome": "Batata Frita Crocante",
     "itens": ["batatas fritas"],
-    "categoria": "",
-    "preco": "",
-    "galeria": [],
+    "categoria": "Acompanhamento",
+    "preco": 10.00,
+    "galeria": [
+      "https://upload.wikimedia.org/wikipedia/commons/8/83/French_Fries.JPG"
+    ],
   },
   {
     "nome": "Onion Rings",
     "itens": ["anéis de cebola empanada"],
-    "categoria": "",
-    "preco": "",
-    "galeria": [],
+    "categoria": "Acompanhamento",
+    "preco": 8.00,
+    "galeria": [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/OnionRings.JPG/649px-OnionRings.JPG"
+    ],
   },
   {
     "nome": "Batata Rústica",
     "itens": ["batatas rústicas assadas", "alecrim", "sal grosso"],
-    "categoria": "",
-    "preco": "",
-    "galeria": [],
+    "categoria": "Acompanhamento",
+    "preco": 12.00,
+    "galeria": [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Roasted_Red_Potatoes_in_box_%2843234169345%29.jpg/640px-Roasted_Red_Potatoes_in_box_%2843234169345%29.jpg"
+    ],
   },
 ];
 
